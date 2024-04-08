e970e8d2022886bb5c24f406e05f2dce29069424
Nerea Barandiarán <barandiaran.138540@e.unavarra.es>
2024-04-08 18:20:44 +0200

    Primer commit, añadiendo kernel

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Añadir clone del repositorio
+#rellenar
+
+#Para analizar el código:
+pmd check -d ./ -R rulesets/java/quickstart.xml -f text -r report.txt
+
+# Para compilar y ejecutar SOLO el kernel:
+mvn clean install -f ./pom.xml -Dmaven.tomcat.home=/opt/tomcat >> maven_kernel.log
+
