3d83fff7f6b73250edd5b966bf995fae2b1dbdef
Nerea Barandiarán <barandiaran.138540@e.unavarra.es>
2024-04-09 14:55:57 +0200

    1.0 release

--- auto.sh
+++ auto.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#Añadir clone del repositorio
-git clone https://github.com/bestShycho/Pliego.git
-
-#Acceder al repo
-cd Pliego
-
 #Para analizar el código:
 pmd check -d ./ -R rulesets/java/quickstart.xml -f text -r report.txt
 
