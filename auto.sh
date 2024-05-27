e9499ef0b20794c442f5ee2d24f8c3daf2dc2154
Nerea Barandiarán <barandiaran.138540@e.unavarra.es>
2024-05-27 16:58:02 +0200

    Anadiendo carpeta de licencias

--- auto.sh
+++ auto.sh
@@ -4,5 +4,19 @@
 pmd check -d ./ -R rulesets/java/quickstart.xml -f text -r report.txt
 
 # Para compilar y ejecutar SOLO el kernel:
-mvn clean install -f ./pom.xml -Dmaven.tomcat.home=/opt/tomcat >> maven_kernel.log
+mvn clean compile install -f ./pom.xml -Dmaven.tomcat.home=/opt/tomcat > maven_kernel.log
 
+# Ir al directorio
+cd $HOME/Escritorio
+
+# Crear directorio para eassybuggy
+mkdir apps-externas
+
+# Ir a la carpeta creada
+cd apps-externas
+
+# Descrgar repositorio eassybuggy
+git clone https://github.com/k-tamura/easybuggy.git
+
+# Para compilar eassybuggy
+mvn clean compile -f easybuggy/pom.xml > maven_easybuggy.log
